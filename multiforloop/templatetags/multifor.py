969e311d2b91ab6d863c9be7a2ae1605e7acf3d7
Tomer Chachamu <tomer.chachamu@gmail.com>
2017-02-24 21:35:21 +0000

    fix import

--- multiforloop/templatetags/multifor.py
+++ multiforloop/templatetags/multifor.py
@@ -2,8 +2,6 @@ from itertools import izip_longest
 
 from django.conf import settings
 
-register = Library()
-
 """
 for x in x_list
 for x,y in xy_list
@@ -15,6 +13,7 @@ for x in x_list; y in y_list reversed
 from django.template import Node, NodeList, Template, Context, Variable, Library
 from django.template import TemplateSyntaxError, VariableDoesNotExist
 
+register = Library()
 
 class ForNode(Node):
     child_nodelists = ('nodelist_loop', 'nodelist_empty')
